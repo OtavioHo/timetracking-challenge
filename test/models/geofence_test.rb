add4cee4299d71a82687cac5502500a0cef22a07
Otavio <otavio@aragoni.com>
2019-03-08 13:30:29 -0300

    add tests

--- test/models/geofence_test.rb
+++ test/models/geofence_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class GeofenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save geofence" do
+    geofence = Geofence.new({"latitude"=>"-23.4971136", "longitude"=>"-46.8877312", "radius"=>"5"})
+    assert geofence.save
+  end
+
+  test "save geofence without lat" do
+    geofence = Geofence.new({ "longitude"=>"-46.8877312", "radius"=>"5"})
+    assert_not geofence.save
+  end
+
+  test "save geofence without lng" do
+    geofence = Geofence.new({ "latitude"=>"-46.8877312", "radius"=>"5"})
+    assert_not geofence.save
+  end
+
+  test "save geofence without radius" do
+    geofence = Geofence.new({"latitude"=>"-23.4971136", "longitude"=>"-46.8877312"})
+    assert_not geofence.save
+  end
 end
--- test/models/timetracking_test.rb
+++ test/models/timetracking_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TimetrackingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save tracknig" do
+    tracking = Timetracking.new({"latitude"=>"-23.4971136", "longitude"=>"-46.8877312", "comments"=>"comemtn"})
+    assert tracking.save
+  end
+
+  test "save tracknig without comments" do
+    tracking = Timetracking.new({"latitude"=>"-23.4971136", "longitude"=>"-46.8877312"})
+    assert tracking.save
+  end
+
+  test "save tracknig without latitude" do
+    tracking = Timetracking.new({"longitude"=>"-46.8877312", "comments"=>"comemtn"})
+    assert_not tracking.save
+  end
+
+  test "save tracknig without longitude" do
+    tracking = Timetracking.new({"latitude"=>"-23.4971136", "comments"=>"comemtn"})
+    assert_not tracking.save
+  end
 end
